08e614c2e011c5a3430c36c606a441f3cc479d73
Pedro <pmctenreiro@gmail.com>
2013-11-10 16:48:07 +0000

    simulador pronto, falta dar jeito ao aspeto

--- sites/all/modules/custom/support/js/support.js
+++ sites/all/modules/custom/support/js/support.js
@@ -1,6 +1,25 @@
 (function($){
   $(document).ready(function(){
-  
+  	 $('.pacote_button').live('mousedown', function(){
+        var str = $(this).attr('class');
+       
+        
+        var substr = str.split(' ');
+        var class_select = substr[1];
+        if(class_select == 'pacote_personalizado')
+        	exit();
+         $('.pacote_button').removeClass('active');
+        $(this).addClass('active');
+        $('#-support-simulador-componentes .form-item input').selected(false);
+        $('#-support-simulador-componentes .form-item input.'+class_select).selected(true);
+
+        
+     }); 
+     $('#-support-simulador-componentes .form-item input').live('change', function(){
+        $('.pacote_button').removeClass('active');
+        $('.pacote_button.pacote_personalizado').addClass('active');
+
+     });
   });
 })(jQuery);
 
